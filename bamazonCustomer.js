b356ced13bdc39026dc8ba83636992bf24537308
Mike <mjeanjacques90@gmail.com>
2019-06-13 00:19:37 -0400

    rebase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "HmYz#4g7p1pH",
+  password: "--redacted--",
   database: "bamazon"
 });
 
